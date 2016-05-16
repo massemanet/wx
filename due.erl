79a6b2b23b4d7d8dba6a2d3c5b72aee52a70c8c5
mats cronqvist <masse@cronqvi.st>
2016-05-16 11:55:15 +0200

    refactor

--- due.erl
+++ due.erl
@@ -11,25 +11,56 @@
 -include_lib("wx/include/wx.hrl").
 
 start() ->
+  Dir = "/Users/masse/git/wx",
+  Files = ["due.xrc","duem.xrc"],
   WX = wx:new(),
-  Xrc = wxXmlResource:get(),
-  wxXmlResource:initAllHandlers(Xrc),
-  wxXmlResource:load(Xrc, "/Users/masse/git/wx/due.xrc"),
-  wxXmlResource:load(Xrc, "/Users/masse/git/wx/duem.xrc"),
-  Frame = wxFrame:new(),
-  wxXmlResource:loadFrame(Xrc, Frame, WX, "MyFrame1"),
-  MenuBar = wxXmlResource:loadMenuBar(Xrc, "m_menubar1"),
-  wxFrame:setMenuBar(Frame,MenuBar),
-  ID1 = wxXmlResource:getXRCID("m_menuitem1"),
-  ID2 = wxXmlResource:getXRCID("m_menuitem2"),
-  wxFrame:connect(Frame,command_menu_selected,[{id,ID1}]),
-  wxFrame:connect(Frame,command_menu_selected,[{id,ID2}]),
-  wxFrame:connect(Frame, close_window),
-  wxFrame:connect(Frame,command_menu_selected, [{id, ?wxID_ANY}]), % -1
-  ButtonID = wxXmlResource:getXRCID(atom_to_list(m_button2)),
-  wxFrame:connect(Frame,command_button_clicked,[{id,ButtonID}]),
+  XRC = resources(Dir,Files),
+  [Frame] = frames(WX,XRC,["MyFrame1"]),
+  menues(XRC,Frame,"m_menubar1",["m_menuItem1","m_menuItem2"]),
+  connect_app_menu(Frame,[?wxID_EXIT]),
+  buttons(Frame,["m_button2"]),
   wxFrame:show(Frame),
   loop().
+
+resources(Dir,Files) ->
+  XRC = wxXmlResource:get(),
+  wxXmlResource:initAllHandlers(XRC),
+  lists:foreach(
+    fun(F) ->
+        wxXmlResource:load(XRC, filename:join(Dir,F))
+    end,
+    Files),
+  XRC.
+
+frames(WX,XRC,FrameNames) ->
+  lists:map(
+    fun(FName)->
+        Frame = wxFrame:new(),
+        wxXmlResource:loadFrame(XRC, Frame, WX, FName),
+        wxFrame:connect(Frame,close_window),
+        Frame
+    end,
+    FrameNames).
+
+connect_app_menu(Frame,Ids) ->
+  [wxFrame:connect(Frame,command_menu_selected,[{id,Id}]) || Id <- Ids].
+
+menues(XRC,Frame,Bar,Items) ->
+  MenuBar = wxXmlResource:loadMenuBar(XRC,Bar),
+  wxFrame:setMenuBar(Frame,MenuBar),
+  connect_items_by_name(Frame,command_menu_selected,Items).
+
+buttons(Frame,Buttons) ->
+  connect_items_by_name(Frame,command_button_clicked,Buttons).
+
+connect_items_by_name(Frame,Command,Items) ->
+  lists:map(
+    fun(Item) ->
+        ID = wxXmlResource:getXRCID(Item),
+        wxFrame:connect(Frame,Command,[{id,ID}]),
+        ID
+    end,
+    Items).
 
 loop() ->
   receive
